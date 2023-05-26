59fea4ac46af343fa15d865a758b262918a3c5b5
serj <makshakov.sergey@vniizht.ru>
2023-05-26 14:34:41 +0300

    example for swagger 2

--- backend/app/handlers/recipes/schemas.py
+++ backend/app/handlers/recipes/schemas.py
@@ -1,24 +1,25 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Union
 
 
 class RecipeSchema(BaseModel):
     page: int
     itemsPerPage: int
-    sortBy: list | None
-    search: dict | None
+    sortBy: Union[list, None] = []
+    search: dict
 
 
 class AddRecipeSchema(BaseModel):
-    title: str
-    ingredients: str
-    instructions: str
-    time: int
-    tags: list[int]
+    title: str = Field(example='Title ...')
+    ingredients: str = Field(example='Ingredients ...')
+    instructions: str = Field(example='Instructions...')
+    time: int = Field(example=1)
+    tags: list[int] = Field(example=[1])
 
 
 class EditRecipeSchema(BaseModel):
-    title: str
-    ingredients: str
-    instructions: str
-    time: float
-    tags: list[int]
+    title: str = Field(example='Edited title ...')
+    ingredients: str = Field(example='Edited ingredients ...')
+    instructions: str = Field(example='Edited instructions...')
+    time: float = Field(example=1)
+    tags: list[int] = Field(example=[1])
